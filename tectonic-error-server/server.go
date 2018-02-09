7d0153632221c66850e15ff93f009aead7c1aa6a
Chance Zibolski <chance.zibolski@coreos.com>
2018-02-09 10:55:26 -0800

    error-server: Set HTTP Response code according to X-Code header
    Default to 404 if unknown X-Code header value is given. Previously the
    server was always returning 200 codes regardless.

--- tectonic-error-server/server.go
+++ tectonic-error-server/server.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"time"
+	"strconv"
 
-	"github.com/coreos-inc/tectonic/ingress-error/binassets"
+	"github.com/coreos/container-images/tectonic-error-server/binassets"
 )
 
 var (
@@ -19,38 +18,57 @@ var (
 	indexPage = binassets.MustAsset("index.html")
 )
 
+type templateData struct {
+	ErrCode int
+	ErrMsg  string
+}
+
 func handleErrorPage(w http.ResponseWriter, r *http.Request) {
-	errorCode := r.Header.Get("X-Code")
-
-	tmpl, _ := template.New("").Parse(string(errorPage))
-
-	var errMsg string
-	switch errorCode {
-	case "400":
-		errMsg = "Bad Request"
-	case "401":
-		errMsg = "Unauthorized Access"
-	case "403":
-		errMsg = "Forbidden"
-	case "404":
-		errMsg = "Not Found"
-	case "500":
-		errMsg = "Internal Server Error"
-	case "503":
-		errMsg = "Service Unavailable"
-	case "504":
-		errMsg = "Gateway Time-out"
-	default:
-		reader := bytes.NewReader(indexPage)
-		http.ServeContent(w, r, r.URL.Path, time.Now(), reader)
+	tmpl, err := template.New("").Parse(string(errorPage))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	data := struct {
-		ErrCode string
-		ErrMsg  string
-	}{errorCode, errMsg}
+	var errorCode int
+	xcodeHeader := r.Header.Get("X-Code")
+	if xcodeHeader != "" {
+		errorCode, err = strconv.Atoi(xcodeHeader)
+		if err != nil {
+			msg := "unable to get error code"
+			data := templateData{500, msg}
+			w.WriteHeader(http.StatusInternalServerError)
+			if err := tmpl.Execute(w, data); err != nil {
+				log.Println(msg)
+			}
+			return
+		}
+	}
 
+	var errMsg string
+	switch errorCode {
+	case 400:
+		errMsg = "Bad Request"
+	case 401:
+		errMsg = "Unauthorized Access"
+	case 403:
+		errMsg = "Forbidden"
+	case 404:
+		errMsg = "Not Found"
+	case 500:
+		errMsg = "Internal Server Error"
+	case 503:
+		errMsg = "Service Unavailable"
+	case 504:
+		errMsg = "Gateway Time-out"
+	default:
+		w.WriteHeader(404)
+		w.Write(indexPage)
+		return
+	}
+
+	data := templateData{errorCode, errMsg}
+	w.WriteHeader(errorCode)
 	if err := tmpl.Execute(w, data); err != nil {
 		log.Printf("Unable to execute template.")
 		return
